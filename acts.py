f2f4e9b4659c19a14ec3ed6cbd1c0c2961c219df
wzkDTerH <694994343@qq.com>
2018-06-01 14:58:46 +0800

    bug fix

--- acts.py
+++ acts.py
@@ -14,13 +14,14 @@ def act_zybkyb(env, agent, T, Tfi):
         states.append(deepcopy(agent.state_now))
         states[-1][x] = 1 - int(states[-1][x])
     values = [agent.agent_arg['ob'](env.getValueFromStates(s, T)) for s in states]
-#    choice = random_choice(norm_softmax(values))
+    #    choice = random_choice(norm_softmax(values))
     choice = max_choice(norm_softmax(values))
     agent.state_now = states[choice]
     agent.RenewRsInfo(agent.state_now,
                       env.getValueFromStates(agent.state_now, T),
                       T)
     return agent
+
 
 def act_xdzx(env, agent, T, Tfi):
     state_next = agent.inter_area.rand_walk(agent.state_now)
@@ -29,12 +30,14 @@ def act_xdzx(env, agent, T, Tfi):
     dE = value_next - value_now
     kT0 = agent.frame_arg['ACT']['xdzx']['kT0']
     cd = agent.frame_arg['ACT']['xdzx']['cool_down']
-    if(dE > 0 or exp(dE / (kT0 * cd**(T+Tfi)) > uniform(0, 1))):
+    if (dE > 0 or exp(dE / (kT0 * cd ** (T + Tfi))) > uniform(0, 1)):
+#        logging.debug("dE:{}, k:{}, p:{}".format(dE, (kT0 * cd ** (T + Tfi)), exp(dE / (kT0 * cd ** (T + Tfi)))))
         agent.state_now = state_next
         agent.RenewRsInfo(agent.state_now,
                           env.getValue(agent.state_now, T),
                           T)
     return agent
+
 
 def act_tscs(env, agent, T, Tfi):
     pass
